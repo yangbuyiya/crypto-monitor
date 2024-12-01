94f4dee5733f5d2142a8c54d1ab8bf04551a5675
shiquda <czy1847259360@gmail.com>
2024-12-01 09:41:25 +0800

    feat: introduce fontawesome

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,10 @@
-import {createApp} from 'vue'
+import { createApp } from 'vue'
 import App from './App.vue'
 import './style.css';
 
+// import Font Awesome CSS
+import "@fortawesome/fontawesome-free/css/all.css";
+import "@fortawesome/fontawesome-free/js/all.js";
+
 createApp(App).mount('#app')
+
